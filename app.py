3bb943bd4c840d85af94685bb75685090bf55783
Renddi Yeh <renddi@relab.cc>
2016-09-01 11:39:54 +0800

    added youtube and behance handler

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, redirect, url_for, jsonify
 import haul
 import validators
 from urllib import unquote
+from urlparse import urlparse, parse_qs
 
 app = Flask(__name__)
 
@@ -12,7 +13,33 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'p7HzB2UBZCxp8B1XGYV10h6
 ###
 # Routing for your application.
 ###
+def video_id(url):
+    """
+    Examples:
+    - http://youtu.be/SA2iWivDJiE
+    - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+    - http://www.youtube.com/embed/SA2iWivDJiE
+    - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+    """
+    query = urlparse(url)
+    if query.hostname == 'youtu.be':
+        return query.path[1:]
+    if query.hostname in ('www.youtube.com', 'youtube.com'):
+        if query.path == '/watch':
+            p = parse_qs(query.query)
+            return p['v'][0]
+        if query.path[:7] == '/embed/':
+            return query.path.split('/')[2]
+        if query.path[:3] == '/v/':
+            return query.path.split('/')[2]
+    # fail?
+    return None
 
+def youtubeImg(url):
+    youtubeID = video_id(url)
+    if youtubeID:
+        return "https://i.ytimg.com/vi/"+youtubeID+"/default.jpg"
+    return None
 @app.route('/')
 def home():
     url = unquote(request.args.get('url'))
@@ -21,8 +48,10 @@ def home():
     if not validators.url(url):
         return jsonify(error=406, text="Invalid url."), 406
 
-    result = haul.find_images(url)
-    return jsonify(result.__dict__)
+    result = haul.find_images(url).__dict__
+    if "YouTube" in result["title"]:
+        result["finder_image_urls"].append(youtubeImg(result["url"]))
+    return jsonify(result)
 
 @app.after_request
 def add_header(response):
